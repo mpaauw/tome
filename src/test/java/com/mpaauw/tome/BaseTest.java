f8868bbe822135ffe33be853da1373cfd1c224f6
mpaauw <matt.paauw@gmail.com>
2020-04-10 17:06:51 -0700

    Fix base test, correct errors in content.

--- src/test/java/com/mpaauw/tome/BaseTest.java
+++ src/test/java/com/mpaauw/tome/BaseTest.java
@@ -3,6 +3,7 @@ package com.mpaauw.tome;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,17 +13,13 @@ public class BaseTest {
     // common tests: not null, not empty string, no upper-case, no whitespace
     @Test
     public void testCommon(String contentUnderTest) {
-        Pattern whitespacePattern = Pattern.compile("\\s");
-        Matcher whitespaceMatcher = whitespacePattern.matcher(contentUnderTest);
-        boolean containsWhitespace = whitespaceMatcher.find();
-
         Pattern uppercasePattern = Pattern.compile("\"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d).+$\"");
         Matcher uppercaseMatcher = uppercasePattern.matcher(contentUnderTest);
         boolean containsUppercase = uppercaseMatcher.find();
 
         Assertions.assertNotNull(contentUnderTest);
         Assertions.assertNotEquals("", contentUnderTest);
-        Assertions.assertFalse(containsWhitespace);
+        Assertions.assertFalse(StringUtils.containsWhitespace(contentUnderTest));
         Assertions.assertFalse(containsUppercase);
     }
 

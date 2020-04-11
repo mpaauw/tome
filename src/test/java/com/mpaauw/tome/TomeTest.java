7bcea4c00401a1c2226f20ef2ffc1fa6167ac11a
mpaauw <matt.paauw@gmail.com>
2020-04-10 22:46:26 -0700

    Refactor tests; fix build issues with maven.

--- src/test/java/com/mpaauw/tome/TomeTest.java
+++ src/test/java/com/mpaauw/tome/TomeTest.java
@@ -1,13 +1,18 @@
 package com.mpaauw.tome;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.platform.commons.util.StringUtils;
 
-public class TomeTest extends BaseTest {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TomeTest {
 
     private Tome tome;
 
-    @BeforeEach
+    @Before
     public void setup() {
         tome = new Tome();
     }
@@ -15,25 +20,45 @@ public class TomeTest extends BaseTest {
     @Test
     public void testGetValidCommonAdjective() {
         String adjective = tome.adjectives().common();
-        this.testCommon(adjective);
-    }
+        Assert.assertNotNull(adjective);
+        Assert.assertNotEquals("", adjective);
+        Assert.assertFalse(containsWhitespace(adjective));
+        Assert.assertFalse(containsUppercase(adjective));    }
 
     @Test
     public void testGetValidCommonColor() {
         String color = tome.colors().common();
-        this.testCommon(color);
-    }
+        Assert.assertNotNull(color);
+        Assert.assertNotEquals("", color);
+        Assert.assertFalse(containsWhitespace(color));
+        Assert.assertFalse(containsUppercase(color));    }
 
     @Test
     public void testGetValidAnimalName() {
         String animal = tome.names().animal();
-        this.testCommon(animal);
+        Assert.assertNotNull(animal);
+        Assert.assertNotEquals("", animal);
+        Assert.assertFalse(containsWhitespace(animal));
+        Assert.assertFalse(containsUppercase(animal));
     }
 
     @Test
     public void testGetValidScientistName() {
         String scientist = tome.names().scientist();
-        this.testCommon(scientist);
+        Assert.assertNotNull(scientist);
+        Assert.assertNotEquals("", scientist);
+        Assert.assertFalse(containsWhitespace(scientist));
+        Assert.assertFalse(containsUppercase(scientist));
+    }
+
+    private boolean containsWhitespace(String content) {
+        return StringUtils.containsWhitespace(content);
+    }
+
+    private boolean containsUppercase(String content) {
+        Pattern uppercasePattern = Pattern.compile("\"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d).+$\"");
+        Matcher uppercaseMatcher = uppercasePattern.matcher(content);
+        return uppercaseMatcher.find();
     }
 
 }
